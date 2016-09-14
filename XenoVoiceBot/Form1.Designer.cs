41b5a1ef125f95f9d5565f68bf87e3a0be31c3ce
Kristijan <Kika_988ng@hotmail.com>
2016-09-14 20:09:15 +0200

    Add files via upload

---
+++ XenoVoiceBot/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace XenoVoiceBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.facebook_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.insta_btn = new System.Windows.Forms.Button();
+            this.youtube_btn = new System.Windows.Forms.Button();
+            this.musicbtn = new System.Windows.Forms.Button();
+            this.hotmailbtn = new System.Windows.Forms.Button();
+            this.proggramingfolder = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(420, 93);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(138, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "VoiceBot";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Xeno";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(239, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(240, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(137, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 7);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Alpha build";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "none";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Status mode:";
+            // 
+            // facebook_btn
+            // 
+            this.facebook_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.facebook_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.facebook_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.facebook_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.facebook_btn.Image = ((System.Drawing.Image)(resources.GetObject("facebook_btn.Image")));
+            this.facebook_btn.Location = new System.Drawing.Point(12, 99);
+            this.facebook_btn.Name = "facebook_btn";
+            this.facebook_btn.Size = new System.Drawing.Size(73, 66);
+            this.facebook_btn.TabIndex = 3;
+            this.facebook_btn.UseVisualStyleBackColor = true;
+            this.facebook_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(362, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Time";
+            // 
+            // insta_btn
+            // 
+            this.insta_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("insta_btn.BackgroundImage")));
+            this.insta_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.insta_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.insta_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insta_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.insta_btn.Location = new System.Drawing.Point(170, 101);
+            this.insta_btn.Name = "insta_btn";
+            this.insta_btn.Size = new System.Drawing.Size(79, 64);
+            this.insta_btn.TabIndex = 5;
+            this.insta_btn.UseVisualStyleBackColor = true;
+            this.insta_btn.Click += new System.EventHandler(this.insta_btn_Click);
+            // 
+            // youtube_btn
+            // 
+            this.youtube_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("youtube_btn.BackgroundImage")));
+            this.youtube_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.youtube_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.youtube_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.youtube_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.youtube_btn.Location = new System.Drawing.Point(323, 99);
+            this.youtube_btn.Name = "youtube_btn";
+            this.youtube_btn.Size = new System.Drawing.Size(75, 66);
+            this.youtube_btn.TabIndex = 6;
+            this.youtube_btn.UseVisualStyleBackColor = true;
+            this.youtube_btn.Click += new System.EventHandler(this.youtube_btn_Click);
+            // 
+            // musicbtn
+            // 
+            this.musicbtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.musicbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("musicbtn.BackgroundImage")));
+            this.musicbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.musicbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.musicbtn.Enabled = false;
+            this.musicbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.musicbtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.musicbtn.Location = new System.Drawing.Point(319, 225);
+            this.musicbtn.Name = "musicbtn";
+            this.musicbtn.Size = new System.Drawing.Size(79, 69);
+            this.musicbtn.TabIndex = 7;
+            this.musicbtn.UseVisualStyleBackColor = false;
+            this.musicbtn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // hotmailbtn
+            // 
+            this.hotmailbtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("hotmailbtn.BackgroundImage")));
+            this.hotmailbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.hotmailbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hotmailbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hotmailbtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.hotmailbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.hotmailbtn.Location = new System.Drawing.Point(164, 227);
+            this.hotmailbtn.Name = "hotmailbtn";
+            this.hotmailbtn.Size = new System.Drawing.Size(85, 64);
+            this.hotmailbtn.TabIndex = 8;
+            this.hotmailbtn.UseVisualStyleBackColor = true;
+            this.hotmailbtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // proggramingfolder
+            // 
+            this.proggramingfolder.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("proggramingfolder.BackgroundImage")));
+            this.proggramingfolder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.proggramingfolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.proggramingfolder.Enabled = false;
+            this.proggramingfolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.proggramingfolder.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.proggramingfolder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.proggramingfolder.Location = new System.Drawing.Point(12, 227);
+            this.proggramingfolder.Name = "proggramingfolder";
+            this.proggramingfolder.Size = new System.Drawing.Size(85, 64);
+            this.proggramingfolder.TabIndex = 9;
+            this.proggramingfolder.UseVisualStyleBackColor = true;
+            this.proggramingfolder.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 352);
+            this.Controls.Add(this.proggramingfolder);
+            this.Controls.Add(this.hotmailbtn);
+            this.Controls.Add(this.musicbtn);
+            this.Controls.Add(this.youtube_btn);
+            this.Controls.Add(this.insta_btn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.facebook_btn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Xeno VoiceBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button facebook_btn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button insta_btn;
+        private System.Windows.Forms.Button youtube_btn;
+        private System.Windows.Forms.Button musicbtn;
+        private System.Windows.Forms.Button hotmailbtn;
+        private System.Windows.Forms.Button proggramingfolder;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ XenoVoiceBot/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Xml;
+using System.Net;
+
+namespace XenoVoiceBot
+{
+    public partial class Form1 : Form
+    {
+        //Dekleracija
+        SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
+        Boolean sleep = true;
+        Choices list = new Choices();
+        public Form1()
+        {
+            list.Add(new String[] { "hey", "how are you","time","date time",
+                "open google","wake up","sleep","open navigation","what is sense of life",
+                "open visual studio","exit","search for global news","check my email","open youtube","open facebook","check my upwork"});
+            //Jokes
+            String[] joke = new string[4] { "A man walks into a library and asks, can I have a cheeseburger the librarian says, sir, this is a library. The man whispers, can I have a cheeseburger?", 
+                " how do you catch a runaway laptop? With an internet.", "what did the zero say to the eight? nice belt.", "time flies like an arrow. fruit flies like a banana." };
+          ///////// //random jokes
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch {  }
+            {
+                s.SelectVoiceByHints(VoiceGender.Female,VoiceAge.Teen);
+               
+                InitializeComponent();
+            }
+        }
+        public void say(String h)
+        {
+            s.Speak(h);
+        }
+        //Pitanja i odgovori
+        private void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+            //sta ja kazem
+            if (r== "wake up")wake=true;   //Budno stanje
+            if (r=="sleep")wake=false;       //Stanje spavanja
+            if (sleep = true) label3.Text = "Sleep"; //label text sleep
+            if (wake==true)
+            {
+                label3.Text = "Wake";                  //label text wake
+            if (r == "hey")
+            {   
+               say("hello sir,how are you today?");
+                //say(list);
+            }
+            if (r == "how are you")
+            {
+                list.Add("great and you");
+                say("great and you?");
+                //say(list);
+            }
+            if (r == "time")
+            {
+                //sati
+                say(DateTime.Now.ToString(" hh:mm tt "));
+            }
+             if (r == "date time")
+            {
+                 //godina
+                say(DateTime.Now.ToString(" M/d/yyyy "));
+            }
+             if (r=="open google")
+             {
+                 say("opening google");
+                 Process.Start("http://google.com");
+             }
+             if (r == "open navigation")
+             {
+                 say("opening navigation");
+                 Process.Start("https://www.google.rs/maps?source=tldso");   
+             }
+             if (r=="what is sense of life")
+             {
+                 say("sense of life is love and money");
+             }
+             if (r == "open visual studio")
+             {
+                 say("opening viusal studio");
+                 Process.Start(@"C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/devenv.exe");
+             }
+             if (r=="exit")
+             {
+                 say("application will close");
+                 this.Close();
+             }
+             if (r=="search for global news")
+             {
+                 say("searching for global news");
+                 Process.Start("https://www.google.rs/search?q=global+news&oq=global+news&aqs=chrome..69i57j0l5.3574j0j4&sourceid=chrome&ie=UTF-8");
+             }
+             if (r == "check my email")
+             {
+                 say("checking mail for you");
+                 Process.Start("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1471374712&rver=6.4.6456.0&wp=MBI_SSL_SHARED&wreply=https:%2F%2Fmail.live.com%2Fdefault.aspx&lc=1033&id=64855&mkt=en-us&cbcxt=mai");
+             
+             }
+             if (r=="open youtube")
+             {
+                 say("opening yotube for you");
+                 Process.Start("http://www.youtube.com");
+             }
+             if (r=="open facebook")
+             {
+                 say("opening facebook for you");
+                 Process.Start("http://facebook.com");
+             }
+             if (r=="check my upwork")
+             {
+                 say("checking upwork for you");
+                 Process.Start("https://www.upwork.com/");
+             }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label6.Text = DateTime.Now.ToString(" hh:mm tt ");
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.facebook.com");
+        }
+
+        private void insta_btn_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.instagram.com");
+        }
+
+        private void youtube_btn_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://youtube.com");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.google.com/gmail/about/");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           // Process.Start(@"C:/Programming,gotove App");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Process.Start(@"C:/Users/Zyzz/Downloads");
+        }
+    }
+}
